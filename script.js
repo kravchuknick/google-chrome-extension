7c4b26da8ae24a929e0a05bfe3b6d06faef368a4
Nick Kravchuk <kravchuknick@gmail.com>
2024-01-25 14:29:24 -0800

    Added font and grid styling

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
 // manifest.json file will contain meta data about the chrome extension
 // Code below is just an example
-
-
-
-// async function fetchData(){
-//     const url = 'https://api-formula-1.p.rapidapi.com/rankings/drivers?season=1991';
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '72dd281243msh0e86cbdb26f31d6p170e3ejsn19a168404eaa',
-//             'X-RapidAPI-Host': 'api-formula-1.p.rapidapi.com'
-//         }
-//     };
-
-//     // try {
-//     //     const res = await fetch(url, options);
-//     //     const result = await res.text();
-//     //     console.log(result);
-//     // } catch (error) {
-//     //     console.error('request error', error);
-//     // }
-
-//     const res = await fetch(url, options);
-//     //const result = await res.text();
-//     const record = await res.json();
-//     console.log('record', record)
-
-//     //document.getElementById("drivers").innerHTML = record.data.map(item => `<li>${item.name}</li>`).join('');
-// }
-
-// fetchData();
+alert("Extension works")
 
 async function fetchData() {
 
@@ -46,13 +17,27 @@ async function fetchData() {
         const response = await fetch(url, options);
         const record = await response.json();
 
-        document.getElementById("drivers").innerHTML = record.response.map(item => `<li>${item.team.name}</li>`).join('');
-
+        document.querySelector("tbody").innerHTML = record.response.map(item => `    <tr>
+                <td><img src = '${item.driver.image}'></td> 
+                <td> ${item.driver.name}</td> 
+                <td> ${item.points}</td> 
+                <td> ${item.wins}</td> 
+                </tr>`).join('');
+                document.querySelector('table').classList.remove('hidden')
+        // document.getElementById("drivers").innerHTML = record.response.map(item => `<li>${item.driver.name}</li>`).join('');
         console.log(record);
     } catch (error) {
         console.error('error', error);
     }
 
 }
-
 fetchData();
+
+let linkTag = document.getElementsByTagName('a');
+console.log(linkTag)
+
+
+
+
+
+
